c273d6e2b50bb5ddefd3c918eba50d94bf6d2b6e
kanish <kanishkanbharathy1999@gmail.com>
2021-07-14 23:47:35 +0530

    Add project files.

---
+++ LibraryManagement/Controllers/ReturnBookController.cs
@@ -0,0 +1,49 @@
+﻿using LibraryManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.SqlServer;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibraryManagement.Controllers
+{
+    public class ReturnBookController : Controller
+    {
+        kctlibraryEntities db = new kctlibraryEntities();
+        // GET: ReturnBook
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetMid(int mid)
+        {
+            var memberid = (from s in db.issuebooks
+                            where s.m_id == mid
+                            select new
+                            {
+                                IssueDate = s.issuedate,
+                                ReturnDate = s.returndate,
+                                Memberid = s.m_id,
+                                BookName = s.book_id,
+                                ElapsDate = SqlFunctions.DateDiff("day", s.returndate, DateTime.Now)
+                            }).ToArray();
+
+            return Json(memberid, JsonRequestBehavior.AllowGet);
+
+        }
+        [HttpPost]
+        public ActionResult Save(returnbook retruns)
+        {
+            if (ModelState.IsValid)
+            {
+                db.returnbooks.Add(retruns);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(retruns);
+
+        }
+    }
+}
---
+++ LibraryManagement/Models/categoryvalidation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LibraryManagement.Models
+{
+    [MetadataType(typeof(categoryMetaData))]
+    public partial class category
+    {
+      
+        public class categoryMetaData
+        {
+            [DisplayName("Category Name")]
+            public string catname { get; set; }
+
+            [DisplayName("Status")]
+            public string status { get; set; }
+        }
+    }
+}
